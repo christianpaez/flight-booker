c38c6fc9447f1e486f247fc2eb7e677f8418aee4
unknown <christianp1894gmail.com>
2020-07-30 17:27:46 -0500

    Rails asset tag && booking routes && responsive layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :bookings
+  resources :bookings, only:[:new, :show]
   resources :flights
   root "flights#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
